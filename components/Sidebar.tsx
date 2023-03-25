1a68925214f9821d06286eb924617bcfc03448b0
Satyajeet Pal <psatyajeet@gmail.com>
2023-03-25 10:31:45 -0400

    Try to style top bar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -24,27 +24,28 @@ export default function Sidebar({
           savedTags.add(tag);
           setSavedTags(savedTags);
           setItem("savedTags", JSON.stringify(Array.from(savedTags)), "local");
-
           setSelectedTag(tag);
           setItem("selectedTag", tag, "local");
         }}
       />
-      <ul className="flex flex-row flex-wrap justify-center my-[24px] sm:flex-wrap lg:flex-col">
-        {Array.from(savedTags).map((tag) => {
-          return (
-            <li
-              className="rounded-md text-lg text-center w-[200px] cursor-pointer py-1 px-2 hover:bg-purple-400 lg:text-left"
-              onClick={() => {
-                setItem("selectedTag", tag, "local");
-                setSelectedTag(tag);
-              }}
-              key={tag}
-            >
-              # {tag}
-            </li>
-          );
-        })}
-      </ul>
+      <div className="">
+        <ul className="flex flex-row flex-wrap overflow-x-auto my-[24px] sm:flex-wrap lg:flex-col">
+          {Array.from(savedTags).map((tag) => {
+            return (
+              <li
+                className="rounded-md text-lg text-center cursor-pointer py-1 px-2 m-[1px] hover:bg-purple-400 lg:text-left"
+                onClick={() => {
+                  setItem("selectedTag", tag, "local");
+                  setSelectedTag(tag);
+                }}
+                key={tag}
+              >
+                # {tag}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
       {/* <ul>
         {topTags.map((tag) => {
           return (
