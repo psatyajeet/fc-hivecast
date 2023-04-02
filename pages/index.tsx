ed889a4d5c2b900453e43e8e4b0c3d3cfa07c9ad
Satyajeet Pal <psatyajeet@gmail.com>
2023-04-02 08:48:18 -0400

    Add meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,26 @@ function Home({ topTags }: { topTags: Array<TagCount> }) {
     <>
       <Head>
         <title>Hashcast</title>
+        <meta name="title" content="Hashcast" />
         <meta name="description" content="Find casts on different topics" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.hashcast.xyz/" />
+        <meta property="og:title" content="Hashcast" />
+        <meta
+          property="og:description"
+          content="Find casts on different topics"
+        />
+        <meta property="og:image" content="" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.hashcast.xyz/" />
+        <meta property="twitter:title" content="Hashcast" />
+        <meta
+          property="twitter:description"
+          content="Find casts on different topics"
+        />
+        <meta property="twitter:image" content="" />
         {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
         <link rel="icon" href="/favicon.ico" />
         <script
